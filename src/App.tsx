46bd24572e473f3dd2425fe38bad45015f68beb5
VladSlob <141657806+VladSlob@users.noreply.github.com>
2025-06-21 10:24:40 +0300

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,56 @@
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import { Loader, TodoFilter, TodoList, TodoModal } from './components';
+import { useDispatch } from 'react-redux';
+import { useEffect, useState } from 'react';
+import { useAppSelector } from './hooks/useAppSelector';
+import { Todo } from './types/Todo';
+import { setTodos } from './features/todos';
+import { getTodos } from './api';
 
-export const App = () => (
-  <>
-    <div className="section">
-      <div className="container">
-        <div className="box">
-          <h1 className="title">Todos:</h1>
+export const App = () => {
+  const dispatch = useDispatch();
+  const todoList = useAppSelector(state => state.todos);
+  const modalTodo = useAppSelector(state => state.currentTodo);
 
-          <div className="block">
-            <TodoFilter />
-          </div>
+  const [loading, setLoading] = useState(false);
 
-          <div className="block">
-            <Loader />
-            <TodoList />
+  useEffect(() => {
+    if (todoList.length > 0) {
+      return;
+    }
+
+    setLoading(true);
+
+    getTodos()
+      .then((response: Todo[]) => dispatch(setTodos(response)))
+      // eslint-disable-next-line no-console
+      .catch(error => console.error(error))
+      .finally(() => setLoading(false));
+  }, [dispatch, todoList]);
+
+  return (
+    <>
+      <div className="section">
+        <div className="container">
+          <div className="box">
+            {loading ? (
+              <Loader />
+            ) : (
+              <>
+                <h1 className="title">Todos:</h1>
+                <div className="block">
+                  <TodoFilter />
+                </div>
+                <div className="block">
+                  <TodoList />
+                </div>
+              </>
+            )}
           </div>
         </div>
       </div>
-    </div>
-
-    <TodoModal />
-  </>
-);
+      {modalTodo && <TodoModal />}
+    </>
+  );
+};
